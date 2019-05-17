c2e0bc34d04e5149b1c61c6a829540150c997006
Andrii Kyselov <ays@melexis.com>
2019-05-17 17:24:05 +0300

    gst-rtsp-server1.0 added

--- sdbuild/packages/custom-settings/qemu.sh
+++ sdbuild/packages/custom-settings/qemu.sh
@@ -4,10 +4,59 @@ set -x
 set -e
 
 cd /root
-
+if [ ! -d "tcf.agent" ]; then
 git clone https://github.com/eclipse/tcf.agent
 cd tcf.agent/agent
 cmake .
 make -j4
 make install
+
+systemctl enable tcf.agent
+fi
+
+exit 0
+
+cd /home/xilinx
+if [ ! -d "gstreamer" ]; then
+git clone https://gitlab.freedesktop.org/gstreamer/gstreamer
+cd gstreamer
+./autogen.sh
+make -j4
+make install
+cd ..
+chown -R xilinx:xilinx gstreamer
+fi
+
+cd /home/xilinx
+if [ ! -d "gst-plugins-base" ]; then
+git clone https://github.com/GStreamer/gst-plugins-base
+cd gst-plugins-base
+./autogen.sh
+make -j4
+cd ..
+chown -R xilinx:xilinx gst-plugins-base
+fi
+
+cd /home/xilinx
+if [ ! -d "gst-plugins-good" ]; then
+git clone https://github.com/GStreamer/gst-plugins-good
+cd gst-plugins-good
+./autogen.sh
+make -j4
+cd ..
+chown -R xilinx:xilinx gst-plugins-good
+fi
+
+cd /home/xilinx
+
+rm -rf gst-rtsp-server
+
+if [ ! -d "gst-rtsp-server" ]; then
+git clone https://github.com/GStreamer/gst-rtsp-server
+cd gst-rtsp-server
+./autogen.sh
+make -j4
+cd ..
+chown -R xilinx:xilinx gst-rtsp-server
+fi
 
